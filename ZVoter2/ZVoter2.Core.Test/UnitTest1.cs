5564297379aaa91d95d97145b610e7063735afe9
jovan.strugar <jovan.strugar@zuehlke.com>
2017-06-01 12:13:49 +0200

    Initial commit.

---
+++ ZVoter2/ZVoter2.Core.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZVoter2.Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private FirstClass _classUnderTest = new FirstClass();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, FirstClass.Sum(1,1));
+        }
+    }
+}
---
+++ ZVoter2/ZVoter2.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZVoter2.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Engineering")]
+[assembly: AssemblyProduct("ZVoter2.Core")]
+[assembly: AssemblyCopyright("Copyright © Zuehlke Engineering 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8b44def-bc9d-4595-9232-74b940e596e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
